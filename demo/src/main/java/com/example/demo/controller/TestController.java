1306fe5d60223f9cfdc30fb56520a0a4babb2ade
Zhaozj <zhaozhangjie@126.com>
2020-03-21 22:17:43 +0800

    第一次测试数据

---
+++ demo/src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+  * 功能描述:   git测试控制器
+  * @return:
+  * @Author: Zhaozj
+  * @Date: 2020/3/21 22:03
+  */
+ @Controller
+public class TestController {
+
+      /**
+       * 功能描述:  测试
+       * @Author: Zhaozj
+       * @Date: 2020/3/21 22:06
+       */
+     public void test(){
+         System.out.println("123");
+     }
+}
